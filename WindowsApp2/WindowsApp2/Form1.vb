5e37ded105556da7dc4cc0428a009d97b4dff676
Paper-blue <2311073425@qq.com>
2023-01-19 18:34:53 +0800

    1.0

---
+++ WindowsApp2/WindowsApp2/Form1.vb
@@ -0,0 +1,19 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim x, As Integer
+        Dim i
+        If x Then
+            For i = 1 To Fix(Len(TextBox1.Text) / 2)
+            If Mid(TextBox1.Text, i, 1) = Mid(TextBox1, (Len(TextBox1) - i) + 1, 1) Then
+                If i = Fix(Len(TextBox1.Text) / 2) Then
+                    Label1("true")
+                End If
+            Else
+                Label1("false")
+                Exit For
+            End If
+        Next
+
+
+    End Sub
+End Class
---
+++ WindowsApp3/WindowsApp3/Form1.vb
@@ -0,0 +1,23 @@
+﻿Public Class Form1
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles Label1.Click
+        Dim a, b, c As Integer
+        Try
+            a = Convert.ToInt32(TextBox1.Text)
+            b = Convert.ToInt32(TextBox2.Text)
+            c = a \ b
+            Label1.Text = c
+        Catch ex1 As DivideByZeroException
+            Label1.Text = "try again"
+        End Try
+        Try
+            Label1.Text c = a \ b
+
+        Catch ex As Exception
+
+        End Try
+    End Sub
+End Class
